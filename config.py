dddae5d1278c54c6d444aa9f12c497c3c2ef6082
olha.shandra <olha.shandra@gmail.com>
2025-04-13 15:10:37 +0300

    path fix

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    APP_DIR = os.path.abspath(os.curdir)
-    WORK_DIR = os.path.join(os.sep, APP_DIR, "app").lstrip(os.sep)
-    RAG_DIR = os.path.join(os.sep, WORK_DIR, "knowledge").lstrip(os.sep)
-    VECTORSTORE_PATH = os.path.join(os.sep, RAG_DIR, "vectorstore").lstrip(os.sep)
+    APP_DIR = os.path.join(os.path.abspath(os.curdir))
+    WORK_DIR = os.path.join(APP_DIR, 'app')
+    RAG_DIR = os.path.join(os.sep, WORK_DIR, 'knowledge')
+    VECTORSTORE_PATH = os.path.join(os.sep, RAG_DIR, 'vectorstore')
 
     DATA_FILE_NAME = f"accessibility_data_with_text.csv"
-    DATA_PATH =  os.path.join(os.sep, RAG_DIR, DATA_FILE_NAME).lstrip(os.sep)
+    DATA_PATH =  os.path.join(os.sep, RAG_DIR, DATA_FILE_NAME)
 
     MODEL = 'gemini-2.0-flash'
     TEMPERATURE = 0.4
